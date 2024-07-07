bb12aef6063d847581197883154702d2f086263a
Moksh Rustagi <rustagimoksh@gmail.com>
2024-07-07 14:55:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import PathFindingVisualizer from "./PathfindingVisualizer/PathFindingVisualizer";
+function App() {
+  return (
+    <div className="App">
+      <PathFindingVisualizer />
+    </div>
+  );
+}
+
+export default App;
